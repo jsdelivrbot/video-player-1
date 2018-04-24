69cd4fa4434d0043f01344ca9b012f79e9010efe
Maria Czerny <mvictoriaczerny@gmail.com>
2018-04-23 21:52:37 -0400

    Add youtube api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import{API_KEY, HOST, PORT} from './key';
+
+const URL = `http:${HOST}:${PORT}/user?${API_KEY}`;
+
 const App = () => {
     return <div>Hello from the other side</div> 
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
