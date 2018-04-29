74d848eaba25445e63e4d50fbdfde809917d3ef3
Maria Czerny <mvictoriaczerny@gmail.com>
2018-04-28 21:34:33 -0400

    show typed search terms to user

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { searchTerm: ' '};
+    }
+
     render() {
-        return <input onChange={input => console.log(input.target.value)}/>;
-    };
-};
+        return (
+            <div>
+                <input 
+                value={this.state.term}
+                onChange={input => this.setState({ searchTerm: input.target.value })}/>
+                Value of the input: {this.state.searchTerm}
+            </div>
+        );
+    }
+}
 
 export default SearchBar; 
