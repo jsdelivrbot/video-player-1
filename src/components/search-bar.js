f794020f1931ddc00566e53b7145ea3be7404e1d
Maria Czerny <mvictoriaczerny@gmail.com>
2018-05-03 19:14:07 -0400

    Add basic css

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input 
                 value={this.state.searchTerm}
                 onChange={input => this.setState({ searchTerm: input.target.value })}/>
