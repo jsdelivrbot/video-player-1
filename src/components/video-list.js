cc1979244f648c745ce42edafe942703723b8a97
Maria Czerny <mvictoriaczerny@gmail.com>
2018-05-02 20:47:08 -0400

    Add key to our videoItem

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -5,7 +5,7 @@ import VideoItem from './video-list-item';
 const VideoList = (props) => {
 
     const videoItems = props.videos.map((video) => {
-        return <VideoItem video={video}/>
+        return <VideoItem key={video.etag} video={video}/>
     });
 
     return (
