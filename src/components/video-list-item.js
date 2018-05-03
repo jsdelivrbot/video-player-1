80fe9033e6c36f6b6892fea4acea581243aa5dcc
Maria Czerny <mvictoriaczerny@gmail.com>
2018-05-02 23:21:03 -0400

    Markup and thumbnails

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-    return <li>Video</li>
+const VideoItem = ({video}) => {
+
+    const image = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={image}/>
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoItem;
